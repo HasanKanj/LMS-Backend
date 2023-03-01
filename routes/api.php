b336d8bdd91530ca826bb13f02ad9541180e75f9
Assala <ismailassala1@gmail.com>
2023-03-01 22:09:33 +0200

    sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+///////////////sanctum auth ///////////////
+//login
+Route::post('/user/login',[UserController::class,'login']);
+//logout
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/user/logout',[UserController::class,'logout']);
+    });
+
+    
 Route::get('/allUser',[UserController::class,'getAllUsers']);
 Route::get('/user/{id}',[UserController::class,'getUserById']);
+
+//register
+// Route::post('/register',[UserController::class,'register']);
+
 Route::post('/user',[UserController::class,'addUser']);
+////////
 Route::put('/user/{id}',[UserController::class,'updateUser']);
 Route::delete('/user/{id}',[UserController::class,'deleteUser']);
 Route::get('/teacher',[UserController::class,'getTeacher']);

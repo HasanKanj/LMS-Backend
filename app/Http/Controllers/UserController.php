1a46c9cc3bc311f05d5576c53dd6fd029e9fc440
hasan <hasankanj2000@gmail.com>
2023-03-10 10:50:40 +0300

    count users length by role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,116 +2,115 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Schema;
-use App\Models\userlms;
+use App\Models\course;
 use App\Models\grade;
 use App\Models\section;
 use App\Models\UserGradeSection;
-use App\Models\course;
+use App\Models\userlms;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 
 class UserController extends Controller
 {
     //get all users
-    public function getAllUsers(Request $request){
+    public function getAllUsers(Request $request)
+    {
         $users = userlms::all();
         return response()->json([
-            'message' => $users
+            'message' => $users,
         ]);
     }
 
     //get user by ID
-        public function getUserById(Request $request, $id){
-            $user = userlms::find($id);
-            return response()->json([
-                'message'=> $user,
-            ]);
-        }
-    
-    //add new user
-    public function addUser(Request $request){
-        $user= new userlms;
-
-        $request->validate([
-            'firstName'=>'required',
-            'lastName'=>'required',
-            'email'=>'required',
-            'password'=>'required',
-            'phoneNumber'=>'required',
-            'role'=>'required',
-        ]);
-        $firstName=$request->input('firstName');
-        $lastName=$request->input('lastName');
-        $email=$request->input('email');
-        $password= Hash::make($request->password);
-        $phoneNumber=$request->input('phoneNumber');
-        $role=$request->input('role');
-
-        $user->firstName=$firstName;
-        $user->lastName=$lastName;
-        $user->email=$email;
-        $user->password=$password;
-        $user->role=$role;
-        $user->phoneNumber=$phoneNumber;
-        $user->save();
-
-////////////////////////////////////////////////////////////////////////////////////////
-        $name= $request->input('name');
-        $letter= $request->input('letter');
-
-        $grade= grade::where('name', $name)->first();
-        $section= section::where('letter', $letter)->first();
-
-         $UserGradeSection = new UserGradeSection;
-         if($user->role == 'student'){
-             $UserGradeSection->student_id = $user->id;
-             $UserGradeSection-> grade_section_id= $grade->sections()->where('section_id', $section->id)->first()->id;
-        
-        $UserGradeSection->grade_section_id;
-        $UserGradeSection->save();
-        
-            }
-
-            else if($user->role == 'teacher'){
-                $UserGradeSection->teacher_id= $user->id;
-                $subject = $request-> input('subject');
-                $course= course::where('subject', $subject)->first();
-                $UserGradeSection->grade_section_id= $grade->sections()->where('section_id', $section->id)->first()->id;
-                $UserGradeSection->grade_section_id;
-                $UserGradeSection-> course_id=$course->id;
-                $UserGradeSection-> course_id;
-                $UserGradeSection->save();
-            }
-        
-/////////////////////////////////////////////////////////////////////////////////////////
-
-
-        $token=$user->createToken('tokenss')->plainTextToken;
-
+    public function getUserById(Request $request, $id)
+    {
+        $user = userlms::find($id);
         return response()->json([
-            'message'=>'DONE!',
-             'token'=>$token,
-             'grade_section'=> $UserGradeSection,
+            'message' => $user,
         ]);
     }
 
+    //add new user
+    public function addUser(Request $request)
+    {
+        $user = new userlms;
+
+        $request->validate([
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'phoneNumber' => 'required',
+            'role' => 'required',
+        ]);
+        $firstName = $request->input('firstName');
+        $lastName = $request->input('lastName');
+        $email = $request->input('email');
+        $password = Hash::make($request->password);
+        $phoneNumber = $request->input('phoneNumber');
+        $role = $request->input('role');
+
+        $user->firstName = $firstName;
+        $user->lastName = $lastName;
+        $user->email = $email;
+        $user->password = $password;
+        $user->role = $role;
+        $user->phoneNumber = $phoneNumber;
+        $user->save();
+
+////////////////////////////////////////////////////////////////////////////////////////
+        $name = $request->input('name');
+        $letter = $request->input('letter');
+
+        $grade = grade::where('name', $name)->first();
+        $section = section::where('letter', $letter)->first();
+
+        $UserGradeSection = new UserGradeSection;
+        if ($user->role == 'student') {
+            $UserGradeSection->student_id = $user->id;
+            $UserGradeSection->grade_section_id = $grade->sections()->where('section_id', $section->id)->first()->id;
+
+            $UserGradeSection->grade_section_id;
+            $UserGradeSection->save();
+
+        } else if ($user->role == 'teacher') {
+            $UserGradeSection->teacher_id = $user->id;
+            $subject = $request->input('subject');
+            $course = course::where('subject', $subject)->first();
+            $UserGradeSection->grade_section_id = $grade->sections()->where('section_id', $section->id)->first()->id;
+            $UserGradeSection->grade_section_id;
+            $UserGradeSection->course_id = $course->id;
+            $UserGradeSection->course_id;
+            $UserGradeSection->save();
+        }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+        $token = $user->createToken('tokenss')->plainTextToken;
+
+        return response()->json([
+            'message' => 'DONE!',
+            'token' => $token,
+            'grade_section' => $UserGradeSection,
+        ]);
+    }
 
     //delete user
-    public function deleteUser(Request $request, $id){
-        $user= userlms::find($id);
+    public function deleteUser(Request $request, $id)
+    {
+        $user = userlms::find($id);
         $user->delete();
 
         return response()->json([
-            'message'=>'DONE! User deleted'
+            'message' => 'DONE! User deleted',
         ]);
 
     }
 
     //update user
-    public function updateUser(Request $request, $id){
-        $user= userlms::find($id);
+    public function updateUser(Request $request, $id)
+    {
+        $user = userlms::find($id);
         $user->fill($request->only([
             'firstName',
             'lastName',
@@ -125,32 +124,48 @@ class UserController extends Controller
             $user->password = Hash::make($request->password);
         }
         $user->save();
-    
+
         return response()->json([
-            'message'=>'DONE! User updated'
+            'message' => 'DONE! User updated',
         ]);
 
     }
-    
-     //get all teachers
-     public function getTeacher(){
+
+    //get all teachers
+    public function getTeacher()
+    {
         $role = "teacher";
         $users = Userlms::where('role', $role)->get();
         return response()->json(['users' => $users]);
     }
 
     //get all students
-    public function getStudents(){
+    public function getStudents()
+    {
         $role = "student";
         $users = Userlms::where('role', $role)->get();
         return response()->json(['users' => $users]);
     }
 
-    //get user by name
-    public function getUserByName($firstName){
-        return userlms:: where('firstName', 'like', '%' .$firstName.'%')->get();
+    public function getUserCountByRole($role)
+    {
+        $users = Userlms::where('role', $role)->get();
+        $count = count($users);
+
+        if ($role === 'student') {
+            return response()->json(['total_students' => $count]);
+        } elseif ($role === 'teacher') {
+            return response()->json(['total_teachers' => $count]);
+        } else {
+            return response()->json(['message' => 'Invalid role']);
+        }
     }
 
+    //get user by name
+    public function getUserByName($firstName)
+    {
+        return userlms::where('firstName', 'like', '%' . $firstName . '%')->get();
+    }
 
     //register
     // public function register(Request $request){
@@ -182,43 +197,40 @@ class UserController extends Controller
     //     return response()->json([
     //         'message'=>'user registered done',
     //         'token'=>$token
-           
+
     //     ]);
     // }
 
-
-
     //login
-    public function login(Request $request){
-        $fields=$request->validate([
-                'email'=>'required',
-                'password'=>'required'
-            ]);
-          
-            //check
-            $user=userlms::where('email',$fields['email'])->first();
-    
-            if(! $user|| ! Hash::check($fields['password'],$user->password)){
-                return response()->json([
-                    'message'=>'Bad creds'
-                ],401);
-            }
-            $token=$user->createToken('token')->plainTextToken;
-    
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        //check
+        $user = userlms::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
             return response()->json([
-                'message'=>'Loggedin Successfully',
-                'token'=>$token,
-            ]);
+                'message' => 'Bad creds',
+            ], 401);
+        }
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Loggedin Successfully',
+            'token' => $token,
+        ]);
     }
 
-
 //logout
-public function logout(Request $request){
-    auth()->user()->tokens()->delete();
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
 
-    return response()->json([ 'message'=> 'Logged out Successfully!!']);
-}
-
-
+        return response()->json(['message' => 'Logged out Successfully!!']);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\GradeSectionController;
 use App\Http\Controllers\SectionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GradeController;
-use App\Http\Controllers\AttendanceController;
-use App\Http\Controllers\GradeSectionController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\UserGradeSectionController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,66 +20,58 @@ use App\Http\Controllers\UserGradeSectionController;
 |
  */
 
- //public
- ///////////////sanctum auth ///////////////
+//public
+///////////////sanctum auth ///////////////
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 //login
-Route::post('/user/login',[UserController::class,'login']);
+Route::post('/user/login', [UserController::class, 'login']);
 
 //logout
 Route::group(['middleware' => ['auth:sanctum']], function () {
-     Route::post('/user',[UserController::class,'addUser']);
-    Route::put('/user/{id}',[UserController::class,'updateUser']);
-    Route::delete('/user/{id}',[UserController::class,'deleteUser']);
-    Route::post('/user/logout',[UserController::class,'logout']);
-    });
+    Route::post('/user', [UserController::class, 'addUser']);
+    Route::put('/user/{id}', [UserController::class, 'updateUser']);
+    Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
+    Route::post('/user/logout', [UserController::class, 'logout']);
+});
 
 ///////////        USER            ///////
-Route::get('/allUser',[UserController::class,'getAllUsers']);
-Route::get('/user/{id}',[UserController::class,'getUserById']);
+Route::get('/allUser', [UserController::class, 'getAllUsers']);
+Route::get('/user/{id}', [UserController::class, 'getUserById']);
 // Route::post('/user',[UserController::class,'addUser']);
 // Route::put('/user/{id}',[UserController::class,'updateUser']);
 // Route::delete('/user/{id}',[UserController::class,'deleteUser']);
-Route::get('/teacher',[UserController::class,'getTeacher']);
-Route::get('/student',[UserController::class,'getStudents']);
-Route::get('/userss/{firstName}',[UserController::class,'getUserByName']);
-
+Route::get('/teacher', [UserController::class, 'getTeacher']);
+Route::get('/student', [UserController::class, 'getStudents']);
+Route::get('/users/{role}', [UserController::class, 'getUserCountByRole']);
+Route::get('/userss/{firstName}', [UserController::class, 'getUserByName']);
 
 /*******Grade ****/
-Route::get('/grade/{id}',[GradeController::class,'getGradeById']);
-Route::get('/grade',[GradeController::class,'getGrade']);
-Route::post('/grade',[GradeController::class,'addGrade']);
-Route::delete('/grade/{id}',[GradeController::class,'deleteGrade']);
-Route::put('/grade/{id}',[GradeController::class,'updateGrade']);
+Route::get('/grade/{id}', [GradeController::class, 'getGradeById']);
+Route::get('/grade', [GradeController::class, 'getGrade']);
+Route::post('/grade', [GradeController::class, 'addGrade']);
+Route::delete('/grade/{id}', [GradeController::class, 'deleteGrade']);
+Route::put('/grade/{id}', [GradeController::class, 'updateGrade']);
 
-
-
-Route::get('/gradeSection/{id}',[GradeSectionController::class,'getGradeSectionById']);
-Route::post('/gradeSection',[GradeSectionController::class,'addGradeSection']);
-Route::delete('/gradeSection/{id}',[GradeSectionController::class,'deleteGradeSection']);
-Route::patch('/gradeSection/{id}',[GradeSectionController::class,'updateGradeSection']);
-
-
-
-
+Route::get('/gradeSection/{id}', [GradeSectionController::class, 'getGradeSectionById']);
+Route::post('/gradeSection', [GradeSectionController::class, 'addGradeSection']);
+Route::delete('/gradeSection/{id}', [GradeSectionController::class, 'deleteGradeSection']);
+Route::patch('/gradeSection/{id}', [GradeSectionController::class, 'updateGradeSection']);
 
 /**********Attendance */
-Route::post('/attendance/{id}',[AttendanceController::class,'createAttendance']);
-Route::get('/attendance',[AttendanceController::class,'getAll']);
-Route::get('/attendance/student/{id}',[AttendanceController::class,'getByStudent']);
-Route::get('/attendance/gradeSection/{id}',[AttendanceController::class,'getByGradeSectionId']);
-
+Route::post('/attendance/{id}', [AttendanceController::class, 'createAttendance']);
+Route::get('/attendance', [AttendanceController::class, 'getAll']);
+Route::get('/attendance/student/{id}', [AttendanceController::class, 'getByStudent']);
+Route::get('/attendance/gradeSection/{id}', [AttendanceController::class, 'getByGradeSectionId']);
 
 /*********Course */
-Route::post('/course',[CourseController::class,'createCourse']);
-Route::get('/course/getAll',[CourseController::class,'getAllCourses']);
-Route::get('/course/{id}',[CourseController::class,'getCourse']);
-Route::delete('deleteById/{id}',[CourseController::class,'deleteById']);
-Route::get('/user-grade-section',[CourseController::class,'index']);
-
+Route::post('/course', [CourseController::class, 'createCourse']);
+Route::get('/course/getAll', [CourseController::class, 'getAllCourses']);
+Route::get('/course/{id}', [CourseController::class, 'getCourse']);
+Route::delete('deleteById/{id}', [CourseController::class, 'deleteById']);
+Route::get('/user-grade-section', [CourseController::class, 'index']);
 
 /*******Section ****/
 Route::Get('/section', [SectionController::class, 'getSection']);
@@ -87,6 +79,3 @@ Route::Get('/section/{id}', [SectionController::class, 'getSingleSection']);
 Route::Post('/addsection', [SectionController::class, 'addSection']);
 Route::Patch('/updatesection/{id}', [SectionController::class, 'updateSection']);
 Route::Delete('/section/{id}', [SectionController::class, 'deleteSection']);
-
-
-

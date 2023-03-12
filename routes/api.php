9129ebc71e29a91cab7caa5a1f5527602d777678
maya-atiah <119121545+maya-atiah@users.noreply.github.com>
2023-03-12 15:44:58 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,11 @@
 <?php
 
 use App\Http\Controllers\AttendanceController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\GradeController;
-use App\Http\Controllers\GradeSectionController;
 use App\Http\Controllers\SectionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GradeController;
-use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\GradeSectionController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserGradeSectionController;
